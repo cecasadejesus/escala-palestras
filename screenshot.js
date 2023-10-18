5f9c7d08a6d5e855c39e19fd7d190606442212bb
Vanessa Me Tonini <vanessametonini@gmail.com>
2023-10-18 18:53:33 -0300

    Script to clean page elements to take a fullsize screenshot

---
+++ screenshot.js
@@ -0,0 +1,32 @@
+wpadminbar.remove()
+masthead.remove()
+secondary.remove()
+colophon.remove()
+
+const author = document.querySelector('.entry-meta');
+const pagination = document.querySelector('.nav-single');
+
+author.remove()
+pagination.remove()
+
+
+const siteArticle = document.querySelector('.site-content article');
+siteArticle.style.marginBottom = "0";
+siteArticle.style.borderBotton = "none";
+
+const pageTopBg = document.querySelector('.post-template-default');
+pageTopBg.style.background = "none";
+
+page.style.background = "none";
+primary.style.width = "100%";
+
+const site = document.querySelector('.site');
+
+const erasePseudoElements = `<style>
+.site:before,
+.site:after {
+	display: none;
+}
+</style>`;
+
+document.body.insertAdjacentHTML('beforeend', erasePseudoElements);
